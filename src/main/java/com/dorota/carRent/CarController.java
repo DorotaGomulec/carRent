152a3df1cf1a25fe9623a4da51b8ffbc63ddface
Dorota Gomulec <doramed8@wp.pl>
2023-10-25 10:35:03 +0200

    CarRepository, CarController: changes

--- src/main/java/com/dorota/carRent/CarController.java
+++ src/main/java/com/dorota/carRent/CarController.java
@@ -2,20 +2,22 @@ package com.dorota.carRent;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class CarController {
-@Autowired
+
     private CarRepository carRepository;
 
+    @Autowired
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
     @GetMapping("/test")
-        public String test(){
+    public String test() {
         return "ok";
     }
 
@@ -24,9 +26,31 @@ public class CarController {
         return carRepository.getAll();
     }
 
+    @GetMapping
+
     @PostMapping("/cars")
-    public int addCar(@RequestBody Car car){
+    public int addCar(@RequestBody Car car) {
         return carRepository.addCar( car );
+    }
+
+    @PatchMapping ("/cars/{id}")
+    public void update(@PathVariable int id, @RequestBody Car updatedCar){
+        Car car = carRepository.getById( id );
+        if(car != null){
+            if(updatedCar.getMark() != null) {
+                car.setMark( updatedCar.getMark() );
+            }
+            if (updatedCar.getModel() !=null){
+                car.setModel( updatedCar.getModel() );
+            }
+        }
+        carRepository.update(car );
+
+    }
+
+    @DeleteMapping ("/cars/{id}")
+    public int delete(@PathVariable int id){
+        return carRepository.delete( id );
     }
 
 }
--- src/main/java/com/dorota/carRent/CarRepository.java
+++ src/main/java/com/dorota/carRent/CarRepository.java
@@ -5,13 +5,17 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
 import java.util.List;
 
 @Repository
 public class CarRepository {
 
+    private JdbcTemplate jdbcTemplate;
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    public CarRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public List<Car> getAll() {
        return jdbcTemplate.query( "SELECT id, mark, model FROM car",
@@ -22,4 +26,20 @@ public class CarRepository {
         jdbcTemplate.update( "INSERT INTO car (mark,model) VALUES(?,?)",car.getMark(),car.getModel());
         return 1;
     }
+
+    public Car getById(int id){
+        return jdbcTemplate.queryForObject( "SELECT id, mark, model FROM car WHERE id=?",
+                BeanPropertyRowMapper.newInstance( Car.class ), id );
+    }
+
+    public int delete(int id){
+        return jdbcTemplate.update( "DELETE FROM car WHERE id=?",id );
+    }
+
+    public void update(Car car) {
+        jdbcTemplate.update( "UPDATE car SET mark=?, model=? WHERE id=?",
+                car.getMark(), car.getModel(), car.getId());
+    }
+
+
 }
