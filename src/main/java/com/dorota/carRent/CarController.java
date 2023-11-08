e99b2e27eae194772c0a8fa9ee92c61d3becbe60
Dorota Gomulec <doramed8@wp.pl>
2023-11-08 13:24:53 +0100

    links to CarController

--- src/main/java/com/dorota/carRent/CarController.java
+++ src/main/java/com/dorota/carRent/CarController.java
@@ -1,13 +1,17 @@
 package com.dorota.carRent;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.stereotype.Controller;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/cars")
 public class CarController {
 
     private CarRepository carRepository;
@@ -22,22 +26,38 @@ public class CarController {
         return "ok";
     }
 
-    @GetMapping("/cars/all")
-    public List<Car> getAll() {
-        return carRepository.getAll();
+    @GetMapping("")
+    public CollectionModel<EntityModel<Car>> getAll() {
+        List<EntityModel<Car>> cars = carRepository.getAll().stream().
+                map( car -> EntityModel.of( car ) ).toList();
+
+        for (EntityModel<Car> car : cars) {
+            int carID = car.getContent().getId();
+            Link link = (Link)  WebMvcLinkBuilder.linkTo(CarController.class).slash( carID ).withSelfRel();
+            Link deleteLink = (Link) WebMvcLinkBuilder.linkTo( CarController.class ).slash( carID ).withRel( "delete-car" ) ;
+            car.add(link,deleteLink);
+
+        }
+        return CollectionModel.of( cars,
+                WebMvcLinkBuilder.linkTo( CarController.class ).withSelfRel() );
     }
 
-    @GetMapping("/cars/{id}")
-    public Car getByID(@PathVariable int id){
-        return carRepository.getById( id );
+    @GetMapping("/{id}")
+    public EntityModel<Car> getByID(@PathVariable int id){
+        return EntityModel.of(carRepository.getById( id ),
+                WebMvcLinkBuilder.linkTo(CarController.class).slash( id ).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(CarController.class).withRel( "all-cars" ),
+                WebMvcLinkBuilder.linkTo(CarController.class).slash( id ).withRel( "delete" )
+                );
+
     }
 
-    @PostMapping("/cars")
+    @PostMapping("")
     public int addCar(@RequestBody Car car) {
         return carRepository.addCar( car );
     }
 
-    @PatchMapping ("/cars/{id}")
+    @PatchMapping ("/{id}")
     public int update(@PathVariable int id, @RequestBody Car updatedCar){
         Car car = carRepository.getById( id );
         if(car != null){
@@ -54,7 +74,7 @@ public class CarController {
         return carRepository.update(car );
     }
 
-    @DeleteMapping ("/cars/{id}")
+    @DeleteMapping ("/{id}")
     public int delete(@PathVariable int id){
         return carRepository.delete( id );
     }
